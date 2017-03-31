a50fb89d250e4853503e466764343b4b6aec00fb
ftondolo <ftondolo@stuy.edu>
2017-03-31 10:47:23 -0400

    Added a few useful methods.

--- QQKachoo.java
+++ QQKachoo.java
@@ -1,2 +1,17 @@
 public class QQKachoo<D> implements Deque<D>{
+    private LLNode<D> _front, _end;
+    // default constructor creates an empty queue
+    public LLQueue()
+        {
+           _front=null;
+           _end=null;
+           }
+    public boolean isEmpty()
+	{
+	  if (_front==_end){
+    		return true;
+  	  }
+ 	 return false;
+	}
+
 }
