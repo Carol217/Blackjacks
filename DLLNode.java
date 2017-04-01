4bbf6581b5b75fc5b075160c25978f6841a60549
Jen Yu <jyu7@stuy.edu>
2017-04-01 14:14:55 -0400

    Added implementation and DLLNode file

---
+++ DLLNode.java
@@ -0,0 +1,91 @@
+/*
+Jennifer Yu 
+APCS2 Period 5
+*/
+/*****************************************************
+ * class DLLNode
+ * Implements a node, for use in lists and other container classes.
+ *****************************************************/
+
+public class DLLNode<T>
+	{
+
+    private T _cargo;    //cargo may only be of type T
+    private DLLNode _nextNode; //pointer to next DLLNode
+    private DLLNode _prevNode; //pointer to previous DLLNode
+
+    // constructor -- initializes instance vars
+    public DLLNode( T value, DLLNode next ) 
+    {
+	_cargo = value;
+	_nextNode = next;
+	_prevNode = null;
+    }
+
+    //--------------v  ACCESSORS  v--------------
+    public T getCargo() { return _cargo; }
+
+    public DLLNode getNext() { return _nextNode; }
+
+    public DLLNode getPrev(){ return _prevNode;}
+    //--------------^  ACCESSORS  ^--------------
+
+
+    //--------------v  MUTATORS  v--------------
+    public T setCargo( T newCargo ) 
+    {
+	T temp = _cargo;
+	_cargo = newCargo;
+	return temp;
+    }
+
+    public DLLNode setNext( DLLNode newNext ) 
+    {
+	DLLNode temp = _nextNode;
+	newNext.setPrev(this);
+	_nextNode = newNext;
+	return temp;
+    }
+
+    public DLLNode setPrev(DLLNode newPrev)
+    {
+	DLLNode temp = _prevNode;
+	_prevNode = newPrev;
+	return temp;
+    }
+    //--------------^  MUTATORS  ^--------------
+
+
+    // override inherited toString
+    public String toString() { return (String) _cargo; }
+
+
+    //main method for testing
+    public static void main( String[] args ) 
+	{
+
+	
+	//Below is an exercise in creating a linked list...
+
+	//Create a node
+	DLLNode first = new DLLNode( "do i", null );
+
+	//Create a new node after the first
+	first.setNext( new DLLNode( "wanna", null ) );
+
+	//Create a third node after the second
+	first.getNext().setNext( new DLLNode( "know", null ) );
+	/*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y) 
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+	//A naive list traversal:
+	while( first != null ) {
+	    System.out.println( "Before: "+ first.getPrev() + " Now: " + first );
+	    first = first.getNext();
+	}
+	
+	System.out.println(first);
+
+	}//end main
+
+    }//end class DLLNode
--- QQKachoo.java
+++ QQKachoo.java
@@ -1,11 +1,12 @@
 public class QQKachoo<D> implements Deque<D>{
-    private LLNode<D> _front, _end;
+    private DLLNode<D> _front, _end;
     // default constructor creates an empty queue
-    public LLQueue()
+    public QQKachoo()
         {
            _front=null;
            _end=null;
            }
+		   
     public boolean isEmpty()
 	{
 	  if (_front==_end){
@@ -13,5 +14,73 @@ public class QQKachoo<D> implements Deque<D>{
   	  }
  	 return false;
 	}
+	
+	public void addFirst(D e)
+	{
+		if (_front == null)
+		{
+			//System.out.println("init");
+			DLLNode<D> tmp = new DLLNode(e, null);
+			_front = tmp;
+			_end = tmp;
+		}
+		else
+		{
+			DLLNode<D> tmp = new DLLNode(e, _front);
+			_front.setPrev(tmp);
+			_front = tmp;
+		}
+	}
+	
+	public void addLast(D e)
+	{
+		if (_front == null)
+		{
+			//System.out.println("init");
+			DLLNode<D> tmp = new DLLNode(e, null);
+			_front = tmp;
+			_end = tmp;
+		}
+		else
+		{
+			DLLNode<D> tmp = new DLLNode(e, null);
+			_end.setNext(tmp);
+			_end = tmp;
+		}
+	}
+	
+	public D removeFirst()
+	{
+		D tmp = (D)_front;
+		_front = _front.getNext();
+		_front.setPrev(null);
+		return tmp;
+	}
+
+    public D removeLast()
+	{
+		D tmp = (D)_end;
+		_end = _end.getPrev();
+		_end.setNext(null);
+		return tmp;
+	}
+
+    public D peekFirst() 
+    {
+		if (!isEmpty())
+		{
+			return (D) _front;
+		}
+		return null;
+    }//O(1)
+
+	public D peekLast() 
+    {
+		if (!isEmpty())
+		{
+			return (D) _end;
+		}
+		return null;
+    }//O(1)
 
 }
